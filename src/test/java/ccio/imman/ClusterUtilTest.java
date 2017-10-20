4bf0fdfbbe03a75406c5c6c4cf2271ca354cc7c6
Eugene Strokin <eugene@strokin.info>
2017-10-20 18:34:39 -0400

    hash

---
+++ src/test/java/ccio/imman/ClusterUtilTest.java
@@ -0,0 +1,12 @@
+package ccio.imman;
+
+import org.junit.Test;
+
+public class ClusterUtilTest {
+
+	@Test
+	public void testCheckProxy(){
+		System.out.println(Math.abs("/pD/y4/fA/bbe0a56e38739c47046b257eac987fd8.jpg".hashCode() % 25));
+		System.out.println("");
+	}
+}
